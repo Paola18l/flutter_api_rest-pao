cf49df912d2b5e8185ae0553f7038a00d318fc1c
Cristipaola18@gmail.com <Cristipaola18@gmail.com>
2024-04-02 16:18:46 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_rest/pages/home_page.dart';
+import 'package:flutter_api_rest/pages/login_page.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_api_rest/pages/register_page.dart';
+import 'package:flutter_api_rest/widgets/login_form.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //------------esta parte sirve para evitar que la aplicacion ajuste
+    //su tamaño a la forma del dispositivo cuando este cambia de posicion
+    //esto sirve para dispositivos moviles tales como telefonos y tablets
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return MaterialApp(
+      home: LoginPage(),
+      routes: {
+        RegisterPage.routeName: (_) => RegisterPage(),
+        LoginPage.routeName: (_) => LoginPage(),
+        HomePage.routeName: (_) => HomePage(),
+      },
+    );
+  }
+}
